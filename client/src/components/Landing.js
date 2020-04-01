2688fecbc65a6e1bda5111d303b9383d960fa188
hillj52 <hillj52@yahoo.com>
2020-04-01 17:19:04 -0400

    first try at pushing client side app to heroku

---
+++ client/src/components/Landing.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+ render() {
+     return (
+        <section style={{ textAlign: 'center' }}>
+            <h2>
+                Emaily!
+            </h2>
+            <h4>
+                Collect feedback from your users
+            </h4>
+        </section>
+     );
+ }
+}
+
+export default Landing;
